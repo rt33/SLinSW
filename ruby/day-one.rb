b3a5aa9688a32994cf48abfdfe0b96520a0a6d88
rt33 <rt33@lastcycle.com>
2016-09-21 03:02:47 +0900

    ruby day-one task

---
+++ ruby/day-one.rb
@@ -0,0 +1,15 @@
+# print Hello world
+puts "Hello world"
+
+# find the index of the word Ruby in "Hello Ruby"
+puts "Hello Ruby".index "Ruby"
+
+# print my name 10 times
+myname = "rt33"
+puts "#{myname}" * 10
+
+# print "this is the sentence x" where x changes from 1 to 10
+10.times do |i|
+  puts "#{i+1}"
+end
+
